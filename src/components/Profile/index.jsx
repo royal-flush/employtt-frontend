6899d65ed771a72009ccc808cbeb097bfd0e2f53
Zachary Orr <zachary.orr@gmail.com>
2019-05-29 06:49:23 -0400

    added format for resume categories to be added

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-//import CardActions from '@material-ui/core/CardActions';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
@@ -29,27 +29,38 @@ class EmployTT extends React.Component {
 
     return (
         <div>
+        <Grid container justify="center" alignItems="center">
           <Card>
             <CardActionArea>
-              <Grid container justify="center" alignItems="center">
-                <Avatar alt="Zachary Orr" src="https://lh3.googleusercontent.com/j0aUONk54i9T5tDzPRVuiHKMyUYkGFbI7vwRKX6b7iDHg8wLrCyldqPMP4hyqb579EjUdEASE3RMMl3wSlx1dvI6uYUB1kJCsVR4x82SJZM8dLSS7fzuu5rBG6XjmLr4o4Ow2Qfga95870Mpby17BT2Onw84tavHat2q674AH2L8fekmUcAxCnLRSdYvJaPgOI1xETNY5ictVBmaE7g0EOzfy8cferRZpuxwQuWuQ_mE6dCmZXL_VIjF_6Y_VrSndD5yMi2iqo2aa67MOODBOgFvYE2NDJsKsNtRJQbVBQZxfaDDZ9ds_LyB5L189sFvvrEpHvF8K_iCBJwnzYl-pGrOrbsrYyEyrdpTuePEKhBBYeqd3g360MkuY3mMeoSGD9iL9I5bTpzqhCMEWi6c9-6CeopSOPSzrnCVryPWqRRuJYWVwI6lypXa5Ju3whf2iVyC7KSOZHqMUy5zF2DYviLFtzicSQL2xaLEfQrAUzi-6KUxY7Tvoxb4bSe5AHml84EW3I4oNE0qY_m66R3nSJpG6evZR9m6In_CcDiwRViFBHnNtTY-5vBBzy6RuJ9ROtY07O7M-u8sbGODpCDtJX5rN50gi1f1HCy2HISShF4kgGegouuAMwVjaAYC7KA6XKtJS1NLaiBQ3KtKOjCoEvayuGIui-jd=w664-h885-no" className={classes.avatar} />        
+              <Avatar alt="Richard Stonebank" src="https://image.shutterstock.com/image-photo/handsome-unshaven-young-darkskinned-male-450w-640011838.jpg" className={classes.avatar} />        
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Zachary Orr
+                  Richard Stonebank
                 </Typography>
                 <Typography variant="body1" component="p">
-                  Address: 41 Sun Valley Avenue, Malabar, Arima
+                  Address: 99 Stock Street, Couva
                 </Typography>
                 <Typography variant="body1" component="p">
-                  Email: <a href="mailto:zachary.orr@gmail.com">zachary.orr@gmail.com</a>
+                  Email: <a href="mailto:Richard.Stonebank@gmail.com">Richard.Stonebank@gmail.com</a>
                 </Typography>
                 <Typography variant="body1" component="p">
-                  Contact Number: +1 868 768 6685
+                  Contact Number: +1 929 555 0144
                 </Typography>
-              </CardContent>
-              </Grid>
+              </CardContent>           
             </CardActionArea>
           </Card>
+        </Grid>
+
+        <Grid container>
+          <Card>
+            <CardActionArea>     
+              <CardContent>
+
+              </CardContent>           
+            </CardActionArea>
+          </Card>
+        </Grid>
+
         </div>
       );
     }
